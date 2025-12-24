5f297c179f4ff59a5f24e932107d5871fc212561
Aliyan Abbas <alabbasi032@gmail.com>
2025-12-23 21:34:49 -0800

    Driving License Eligibility Checker (C++)
    This project is a simple C++ program that checks whether a person is eligible for a driving license based on their age. The program takes the user’s age as input and determines if they meet the minimum age requirement for obtaining a driving license.

---
+++ driving_license.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout<<"Enter your age=";
+    cin>>age;
+    if(age>=18){
+        cout<<"you are eligible for apply driving license ";
+    }else{
+        cout<<"you are not eligible for apply driving license ";
+    }
+}
